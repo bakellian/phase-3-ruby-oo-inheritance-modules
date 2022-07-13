42e2c663951a2f9cb64a60c87d2e63c5ce19fec1
Bridget Akellian <akellianbv@gmail.com>
2022-07-13 10:05:10 -0400

    commit tree 74b5d11e203082e4f4fc4640338c6f8620c185ae

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,23 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+require_relative './fancy_dance'
+
+require 'pry'
+
+class Dancer
+
+    extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+
+end
+
+angelina = Dancer.new
+angelina.twirl
+
+binding.pry
